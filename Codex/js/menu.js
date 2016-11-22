655b68239952f4578c5cb6c0ac6c4a481f7ba1b6
airsick <colerbird@gmail.com>
2016-11-21 22:28:58 -0500

    Add files via upload

---
+++ Codex/js/menu.js
@@ -0,0 +1,22 @@
+$(window).load(function() {
+	menu.init();
+});
+
+var menu = {
+	// Show menu screen
+	init: function() {
+		$('.gamelayer').hide();
+		$('#mainmenu').hide();
+		$('#menuscreen').show();
+	},
+
+	titleClick: function() {
+		$('#title').hide();
+		$('#mainmenu').show();
+	},
+
+	singleplayerClick: function() {
+		$('#menuscreen').hide();
+		$('#gamescreen').show();
+	}
+}
